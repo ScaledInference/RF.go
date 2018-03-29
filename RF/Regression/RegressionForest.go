cd6ed7b38dbc38499785cdf22352208ae7f6a295
joey-si <joey@scaledinference.com>
2018-03-29 12:42:25 -0700

    Make deterministic and get rid of categorical features

--- RF/Regression/RegressionForest.go
+++ RF/Regression/RegressionForest.go
@@ -13,8 +13,7 @@ type Forest struct{
 	Trees []*Tree
 }
 
-func BuildForest(inputs [][]interface{},labels []float64, treesAmount, samplesAmount, selectedFeatureAmount int) *Forest{
-	rand.Seed(time.Now().UnixNano())
+func BuildForest(rs rand.Source, inputs [][]float64,labels []float64, treesAmount, samplesAmount, selectedFeatureAmount int) *Forest{
 	forest := &Forest{}
 	forest.Trees = make([]*Tree,treesAmount)
 	done_flag := make(chan bool)
@@ -23,11 +22,11 @@ func BuildForest(inputs [][]interface{},labels []float64, treesAmount, samplesAm
 	for i:=0;i<treesAmount;i++{
 		go func(x int){
 			fmt.Printf(">> %v buiding %vth tree...\n", time.Now(), x)
-			forest.Trees[x] = BuildTree(inputs,labels,samplesAmount,selectedFeatureAmount)
+			forest.Trees[x] = BuildTree(rs, inputs,labels,samplesAmount,selectedFeatureAmount)
 			//fmt.Printf("<< %v the %vth tree is done.\n",time.Now(), x)
 			mutex.Lock()
 			prog_counter+=1
-			fmt.Printf("%v tranning progress %.0f%%\n",time.Now(),float64(prog_counter) / float64(treesAmount)*100) 
+			fmt.Printf("%v tranning progress %.0f%%\n",time.Now(),float64(prog_counter) / float64(treesAmount)*100)
 			mutex.Unlock()
 			done_flag <- true
 		}(i)
@@ -41,13 +40,13 @@ func BuildForest(inputs [][]interface{},labels []float64, treesAmount, samplesAm
 	return forest
 }
 
-func DefaultForest(inputs [][]interface{},labels []float64, treesAmount int) *Forest{
-	m := int( math.Sqrt( float64( len(inputs[0]) ) ) ) 
+func DefaultForest(rs rand.Source, inputs [][]float64,labels []float64, treesAmount int) *Forest{
+	m := int( math.Sqrt( float64( len(inputs[0]) ) ) )
 	n := int( math.Sqrt( float64( len(inputs) ) )  )
-	return BuildForest(inputs,labels, treesAmount,n,m)
+	return BuildForest(rs, inputs,labels, treesAmount,n,m)
 }
 
-func (self *Forest) Predicate(input []interface{}) float64{
+func (self *Forest) Predicate(input []float64) float64{
 	total := 0.0
 	for i:=0;i<len(self.Trees);i++{
 		total += PredicateTree(self.Trees[i],input)
--- RF/Regression/RegressionTree.go
+++ RF/Regression/RegressionTree.go
@@ -4,14 +4,12 @@ package Regression
 import (
 	"math/rand"
 	//"fmt"
+	"math"
 )
-
-const CAT = "cat"
-const NUMERIC = "numeric"
 
 type TreeNode struct{
 	ColumnNo int //column number
-	Value interface{}
+	Value *float64
 	Left *TreeNode
 	Right *TreeNode
 	Label float64
@@ -21,22 +19,23 @@ type Tree struct{
 	Root *TreeNode
 }
 
-func getRandomRange(N int, M int) []int{
+func getRandomRange(rs rand.Source, N int, M int) []int{
 	tmp := make([]int,N)
 	for i:=0;i<N;i++{
 		tmp[i]=i
 	}
+	rr := rand.New(rs)
 	for i:=0;i<M;i++{
-		j := i + int(rand.Float64()*float64(N-i))
+		j := i + int(rr.Float64()*float64(N-i))
 		tmp[i],tmp[j] = tmp[j],tmp[i]
 	}
 
 	return tmp[:M]
 }
 
-func getSamples(ary [][]interface{}, index []int)  [][]interface{} {
+func getSamples(ary [][]float64, index []int)  [][]float64 {
 	//fmt.Println("ary",ary)
-	result := make([][]interface{}, len(index))
+	result := make([][]float64, len(index))
 	for i:=0;i<len(index);i++{
 		result[i] = ary[index[i]]
 	}
@@ -71,13 +70,13 @@ func getMSE(labels []float64) float64 {
 }
 
 
-func getBestGain(samples [][]interface{}, c int, samples_labels []float64, column_type string,current_mse float64) (float64,interface{},int,int){
-	var best_value interface{}
+func getBestGain(samples [][]float64, c int, samples_labels []float64,current_mse float64) (float64,float64,int,int){
+	var best_value float64
 	best_gain := 0.0
 	best_total_r := 0
 	best_total_l := 0
 
-	uniq_values := make(map[interface{}]int)
+	uniq_values := make(map[float64]int)
 	for i:=0;i<len(samples);i++{
 		uniq_values[samples[i][c]] = 1
 	}
@@ -86,27 +85,15 @@ func getBestGain(samples [][]interface{}, c int, samples_labels []float64, colum
 		labels_l := make([]float64,0)
 		labels_r := make([]float64,0)
 		total_l := 0
-		total_r := 0 
-		if column_type==CAT{
-			for j:=0;j<len(samples);j++{
-				if samples[j][c]==value{
-					total_l += 1
-					labels_l = append(labels_l,samples_labels[j])
-				}else{
-					total_r += 1
-					labels_r = append(labels_r,samples_labels[j])
-				}
-			}
-		}
-		if column_type==NUMERIC{
-			for j:=0;j<len(samples);j++{
-				if samples[j][c].(float64)<=value.(float64){
-					total_l += 1
-					labels_l = append(labels_l,samples_labels[j])
-				}else{
-					total_r += 1
-					labels_r = append(labels_r,samples_labels[j])
-				}
+		total_r := 0
+
+		for j:=0;j<len(samples);j++{
+			if samples[j][c] <=value {
+				total_l += 1
+				labels_l = append(labels_l,samples_labels[j])
+			}else{
+				total_r += 1
+				labels_r = append(labels_r,samples_labels[j])
 			}
 		}
 
@@ -117,7 +104,7 @@ func getBestGain(samples [][]interface{}, c int, samples_labels []float64, colum
 
 		//fmt.Println(new_mse,part_l,part_r)
 		mse_gain := current_mse - new_mse
-		
+
 		if mse_gain>=best_gain{
 			best_gain = mse_gain
 			best_value = value
@@ -129,29 +116,18 @@ func getBestGain(samples [][]interface{}, c int, samples_labels []float64, colum
 	return best_gain, best_value, best_total_l,best_total_r
 }
 
-func splitSamples(samples [][]interface{}, column_type string, c int, value interface{}, part_l *[]int, part_r *[]int){
-		if column_type==CAT{
-			for j:=0;j<len(samples);j++{
-				if samples[j][c]==value{
-					*part_l = append(*part_l,j)
-				}else{
-					*part_r = append(*part_r,j)
-				}
-			}
+func splitSamples(samples [][]float64, c int, value float64, part_l *[]int, part_r *[]int){
+	for j:=0;j<len(samples);j++{
+		if samples[j][c] <=value {
+			*part_l = append(*part_l,j)
+		}else{
+			*part_r = append(*part_r,j)
 		}
-		if column_type==NUMERIC{
-			for j:=0;j<len(samples);j++{
-				if samples[j][c].(float64)<=value.(float64){
-					*part_l = append(*part_l,j)
-				}else{
-					*part_r = append(*part_r,j)
-				}
-			}
-		}
+	}
 }
 
 
-func buildTree(samples [][]interface{}, samples_labels []float64, selected_feature_count int) *TreeNode{
+func buildTree(rs rand.Source, samples [][]float64, samples_labels []float64, selected_feature_count int) *TreeNode{
 	//fmt.Println(len(samples))
 	//find a best splitter
 	//fmt.Println(samples)
@@ -159,34 +135,29 @@ func buildTree(samples [][]interface{}, samples_labels []float64, selected_featu
 	column_count := len(samples[0])
 	//split_count := int(math.Log(float64(column_count)))
 	split_count := selected_feature_count
-	columns_choosen := getRandomRange(column_count,split_count)
-	
+	columns_choosen := getRandomRange(rs, column_count,split_count)
+
 	best_gain := 0.0
 	var best_part_l []int = make([]int,0,len(samples))
 	var best_part_r []int = make([]int,0,len(samples))
-	var best_value interface{}
+	var best_value *float64
 	var best_column int
-	var best_total_l int = 0 
+	var best_total_l int = 0
 	var best_total_r int = 0
-	var best_column_type string
 
 	current_mse := getMSE(samples_labels)
 
 	for _,c := range columns_choosen{
-		column_type := CAT
-		if _,ok := samples[0][c].(float64) ; ok{
-			column_type = NUMERIC
-		}
 		//fmt.Println(column_type)
-		gain,value,total_l,total_r := getBestGain(samples,c,samples_labels,column_type,current_mse)
+		gain,value,total_l,total_r := getBestGain(samples,c,samples_labels,current_mse)
 		//fmt.Println("kkkkk",gain,part_l,part_r)
 		if gain>=best_gain{
 			best_gain = gain
 			best_total_l = total_l
 			best_total_r = total_r
-			best_value = value
+			v := value
+			best_value = &v
 			best_column = c
-			best_column_type = column_type
 		}
 	}
 
@@ -195,14 +166,14 @@ func buildTree(samples [][]interface{}, samples_labels []float64, selected_featu
 		node := &TreeNode{}
 		node.Value = best_value
 		node.ColumnNo = best_column
-		splitSamples(samples, best_column_type, best_column, best_value,&best_part_l,&best_part_r)
-		node.Left = buildTree(getSamples(samples,best_part_l),getLabels(samples_labels,best_part_l), selected_feature_count)
-		node.Right = buildTree(getSamples(samples,best_part_r),getLabels(samples_labels,best_part_r), selected_feature_count)
+		splitSamples(samples, best_column, *best_value, &best_part_l,&best_part_r)
+		node.Left = buildTree(rs, getSamples(samples,best_part_l),getLabels(samples_labels,best_part_l), selected_feature_count)
+		node.Right = buildTree(rs, getSamples(samples,best_part_r),getLabels(samples_labels,best_part_r), selected_feature_count)
 		return node
 	}
-		
+
 	return genLeafNode(samples_labels)
-	
+
 }
 
 func genLeafNode(labels []float64) *TreeNode{
@@ -218,7 +189,7 @@ func genLeafNode(labels []float64) *TreeNode{
 }
 
 
-func predicate(node *TreeNode, input []interface{}) float64{
+func predicate(node *TreeNode, input []float64) float64 {
 	//fmt.Println("node",node)
 	if node.Value == nil{ //leaf node
 		return node.Label
@@ -227,42 +198,33 @@ func predicate(node *TreeNode, input []interface{}) float64{
 	c := node.ColumnNo
 	value := input[c]
 
-	switch value.(type){
-	case float64:
-		if value.(float64)<=node.Value.(float64) && node.Left!=nil{
-			return predicate(node.Left,input)
-		}else if node.Right!=nil{
-			return predicate(node.Right,input)
-		}
-	default:
-		if value==node.Value && node.Left!=nil{
-			return predicate(node.Left,input)
-		}else if node.Right != nil{
-			return predicate(node.Right,input)
-		}
+	if value <= *(node.Value) && node.Left!=nil{
+		return predicate(node.Left,input)
+	} else if node.Right!=nil{
+		return predicate(node.Right,input)
 	}
-
-	return 0
+	return math.NaN()
 }
 
 
-func BuildTree(inputs [][]interface{}, labels []float64, samples_count,selected_feature_count int) *Tree{
-	samples := make([][]interface{},samples_count)
+func BuildTree(rs rand.Source, inputs [][]float64, labels []float64, samples_count,selected_feature_count int) *Tree{
+	samples := make([][]float64,samples_count)
 	samples_labels := make([]float64,samples_count)
+	rr := rand.New(rs)
 	for i:=0;i<samples_count;i++{
-		j := int(rand.Float64()*float64(len(inputs)))
+		j := int(rr.Float64()*float64(len(inputs)))
 		samples[i] = inputs[j]
 		samples_labels[i] = labels[j]
 	}
 
 	//fmt.Println(samples)
 	tree := &Tree{}
-	tree.Root = buildTree(samples,samples_labels, selected_feature_count)
+	tree.Root = buildTree(rs, samples,samples_labels, selected_feature_count)
 	return tree
 }
 
 
 
-func PredicateTree(tree *Tree, input []interface{}) float64{
+func PredicateTree(tree *Tree, input []float64) float64{
 	return predicate(tree.Root,input)
 }
